bd0a23e2a25fb5e2abb2f339aa11e4f8a7859b08
Amitbhivsane <amitbhivsane@gmail.com>
2025-07-08 17:50:49 +0530

    eight commit

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -4,7 +4,7 @@ const API = axios.create({
   baseURL:
     typeof window !== "undefined" && window.location.hostname === "localhost"
       ? "http://localhost:5001/api"
-      : "https://your-backend-service.onrender.com/api", // ✅ Replace with real URL
+      : "/api", // ✅ Replace with real URL
 });
 
 // Transactions
